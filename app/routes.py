fd6824493356fb4bca81d5ea5cf96e57b6c9f554
WillCPP <WillCPP@users.noreply.github.com>
2018-03-13 15:40:52 -0400

    Initial Commit

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from app import app
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    appid = 'c00636f4e12a151f9695c2ca582f9521'
+
+    send_url_g = 'http://freegeoip.net/json'
+    r_g = requests.get(send_url_g)
+    j_g = json.loads(r_g.text)
+    lat = j_g['latitude']
+    lon = j_g['longitude']
+
+    send_url_w = 'http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}'.format(lat, lon, appid)
+    r_w = requests.get(send_url_w)
+    j_w = json.loads(r_w.text)
+    weather = j_w['weather'][0]
+    print(j_w['weather'])
+    return "<head><title>W E A T H E R</title></head><body><h1>Hello World!</h1><p>" + weather['main'] + ": " + weather['description'] + "</p></body>" 
