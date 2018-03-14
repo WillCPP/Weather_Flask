198513f0f9d2780c804652d819b5ae56cc23663c
WillCPP <WillCPP@users.noreply.github.com>
2018-03-14 14:26:29 -0400

    Change font to Lato

--- WeatherHTML.py
+++ WeatherHTML.py
@@ -1,4 +1,4 @@
-one = "<html><head><title>W E A T H E R</title><style>div {width: 80%;background: white;text-align: center;margin: 0 auto;margin-top: 100px;}</style></head><body><div><p><h1>The weather is probably</h1></p><img src=\"static/images/"
+one = "<html><head><title>W E A T H E R</title><link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'><style>div {width: 80%;background: white;text-align: center;margin: 0 auto;margin-top: 100px;} html {font-family: 'Lato';font-size: 22px;}</style></head><body><div><p><h1>The weather is probably</h1></p><img src=\"static/images/"
 
 two = ".svg\"><p><h1>"
 
