dad3588c633f506d358b78cb6fa77e4d3793a06b
WillCPP <WillCPP@noreply.users.github.com>
2018-03-15 00:03:15 -0400

    Add background animation

--- WeatherHTML.py
+++ WeatherHTML.py
@@ -1,5 +1,5 @@
-one = "<html><head><title>W E A T H E R</title><link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'><style>div {width: 80%;background: white;text-align: center;margin: 0 auto;margin-top: 100px;} html {font-family: 'Lato';font-size: 22px;}</style></head><body><div><p><h1>The weather is probably</h1></p><img src=\"static/images/"
+one = """<html><head><title>W E A T H E R</title><link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'><link rel="stylesheet" type="text/css" href="static/styles/style.css"></head><body><div><p><h1>The weather is probably</h1></p><img src="static/images/"""
 
-two = ".svg\"><p><h1>"
+two = """.svg"><p><h1>"""
 
-three = "</h1></p></div></body></html>"
+three = """</h1></p></div></body></html>"""
